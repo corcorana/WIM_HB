15aa9312d86ab705c3045926a3a8cb2a9c009d65
corcorana <andrew.corcoran@monash.edu>
2025-05-01 23:32:46 +1000

    prep for osf

--- localdef_WIM_HB.m
+++ localdef_WIM_HB.m
@@ -1,12 +1,9 @@
 %% set local paths to raw data & MATLAB toolboxes
 
-% Melbourne dataset [download from https://osf.io/ey3ca/]
+% Melbourne raw data [download from https://osf.io/ey3ca/]
 path_mbi_raweeg = 'path_to_Raw_EEG_Data';
 path_mbi_raweye = 'path_to_Raw_Eye-Tracking_Data';
 path_mbi_behav  = 'path_to_Behavioural_Data';
-
-% Paris dataset [download from ???]
-path_pbi_rawdat = 'path_to_Paris_Data';
 
 % MATLAB path & toolboxes
 path_mat    = 'C:\Users\corcoraa\Documents\MATLAB\';
@@ -19,7 +16,7 @@ path_TFCE   = [path_mat, 'ept_TFCE-matlab-master']; 	% https://github.com/Mensen
 path_nmd    = [path_mat, 'NMDv2-00']; 			% http://www.physics.lancs.ac.uk/research/nbmphysics/diats/nmd/
 path_pncst  = [path_mat, 'PhysioNet-Cardiovascular-Signal-Toolbox']; 	% https://physionet.org/content/pcst/1.0.0/
 
-% Directory for preprocessed files
+% Directory for preprocessed files [download Paris data from https://osf.io/v9xsw/]
 wim_preproc = fullfile(pwd, 'preproc_dat');
 if ~exist(wim_preproc, 'dir')
     warning(['Unable to find ', wim_preproc, '... creating new directory'])
